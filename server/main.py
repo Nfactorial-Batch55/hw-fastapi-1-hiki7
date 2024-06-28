b8c065efe55f56c6ddf585326bd14a4d6b638a15
hiki7 <rsaliyev03@gmail.com>
2024-06-28 13:37:58 +0500

    server-ping-pong and server-request-info are solved

--- server/main.py
+++ server/main.py
@@ -1,6 +1,22 @@
 def app(environ, start_response):
-    data = b"Hello, World!\n"
-    start_response(
-        "200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
-    )
-    return iter([data])
+    path = environ.get('PATH_INFO')
+    method = environ.get('REQUEST_METHOD')
+
+    if method == 'GET' and path == '/ping':
+        data = b"pong\n"
+        start_response(
+            "200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
+        )
+        return iter([data])
+    elif method == 'GET' and path == '/info':
+        data = f"Method: {method}\nURL: {environ.get('PATH_INFO')}\nProtocol: {environ.get('SERVER_PROTOCOL')}\n".encode('utf-8')
+        start_response(
+            "200 OK", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
+        )
+        return iter([data])
+    else:
+        data = b"Not Found\n"
+        start_response(
+            "404 Not Found", [("Content-Type", "text/plain"), ("Content-Length", str(len(data)))]
+        )
+        return iter([data])
